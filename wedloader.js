26018abfb600d4d0290d356f3df1229d53ad95b2
daniel7932 <166502658+Daniel7932@users.noreply.github.com>
2025-12-31 15:33:05 +0800

    Update wedloader.js

--- wedloader.js
+++ wedloader.js
@@ -5,7 +5,7 @@ javascript:(function(){
     /* 2. 建立 Script 標籤來載入外部程式碼 */
     var script = document.createElement('script');
     /* ↓↓↓ 下面這行換成你在 raw.githack.com 產生的網址 ↓↓↓ */
-    script.src = 'https://raw.githack.com/使用者帳號/專案名稱/分支/auto-refresh.js'; 
+    script.src = 'https://shorturl.at/KRKtj'; 
     /* ↑↑↑↑↑↑ */
     
     /* 3. 避免重複載入 */
